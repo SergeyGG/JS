0e85efd6f2245a804e4f7e299e36ba4e530f9557
SergeyGG <1letterssk@gmail.com>
2016-07-11 07:01:12 +0300

    final

--- field/js/app.js
+++ field/js/app.js
@@ -1,6 +1,45 @@
+function tableCreate(){
+    var body = document.body,
+        tbl  = document.createElement('table');
+    tbl.classList.add("table");
+    
+    for(var i = 0; i <= 10; i++){
+        var tr = tbl.insertRow();
+        for(var j = 0; j <= 10; j++){
+            var td = tr.insertCell();
+            td.classList.add("green");
+        }
+    }
+
+    body.appendChild(tbl);
+}
+
+tableCreate();
+
+window.onload = function() {
+	var td = document.getElementsByTagName("td");
+	for (var i = 0; i < td.length; i++) {
+		td[i].onmousedown = changeColor;
+	}
+
+	function changeColor(event) {
+	var td = event.target;
+	td.classList.remove("green");
+	td.classList.add("white");
+	}
+
+	change.onclick = function replaceClass() {
 
 
+		var td = document.getElementsByTagName("td");
+		console.log (td);
+		for (var i = 0; i < td.length; i++) {
+			if (td[i].className == "white") {
+				td[i].className == "green";
+			}
+			
+		}
 
-
-
-
+	}
+  
+}
